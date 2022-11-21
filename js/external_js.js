b7d97171c5f264b6ca852b2e12ecbb37daff2876
Jason Crites <jcrites3@hotmail.com>
2022-11-21 09:56:09 -0800

    added html js and js directory

--- js/external_js.js
+++ js/external_js.js
@@ -1,4 +1,4 @@
 console.log("Hello from External Javascript");
 alert("Welcome to my Webpage");
 var userPrompt = prompt("What's your favorite color");
-alert(userPrompt + "?" + " Wow mine toko");
+alert(userPrompt + "?" + " Wow mine too");
